355f4dca2139839497e66b9d8c95a9b7c089c621
Bob <heineman.bob@gmail.com>
2016-10-29 02:21:16 -0500

    add personal file

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@ apt-get update
 apt-get install -y apt-transport-https ca-certificates
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo 'deb https://apt.dockerproject.org/repo ubuntu-xenial main' | tee /etc/apt/sources.list.d/docker.list
-apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual
+apt-get install -y linux-image-extra-$(uname -r) linux-image-extra-virtual
 apt-get update
-apt-get install docker-engine
+apt-get install -y docker-engine
 service docker start
-apt-get install docker-compose
-apt-get install git zsh
+apt-get install -y docker-compose
+apt-get install -y git zsh
